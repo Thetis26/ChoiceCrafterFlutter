4aff9811709699187f2cb7931a0360a8295a0230
Thetis26 <57312795+Thetis26@users.noreply.github.com>
2026-01-31 13:15:30 +0200

    Add iOS weekly usage export worker

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,4 +1,6 @@
+import BackgroundTasks
 import Flutter
+import FirebaseCore
 import UIKit
 
 @main
@@ -8,6 +10,16 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    if FirebaseApp.app() == nil {
+      FirebaseApp.configure()
+    }
+    WeeklyUsageExportWorker.register()
+    WeeklyUsageExportWorker.schedule()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func applicationDidEnterBackground(_ application: UIApplication) {
+    super.applicationDidEnterBackground(application)
+    WeeklyUsageExportWorker.schedule()
   }
 }
